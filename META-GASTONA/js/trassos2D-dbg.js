ed77a161307ab3df77a319772abec728132a1b04
alexawake <appdev101@wakeupthecat.com>
2020-11-13 01:00:56 +0100

    Add documents and missing js

---
+++ META-GASTONA/js/trassos2D-dbg.js
@@ -0,0 +1,12 @@
+/*
+Copyright (C) 2015-2020 Alejandro Xalabarder Aulet
+License : GNU General Public License (GPL) version 3
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*/
+
+@<:infile META-GASTONA/js/vec3.js>
+@<:infile META-GASTONA/js/canvasSync.js>
+@<:infile META-GASTONA/js/trassos2D.js>
